796de5fcd65f43a3be5887957a087943c9bb8f1b
zhengerwei <515897860@qq.com>
2021-12-30 14:07:17 +0800

    feat: 更新store

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -1,14 +1,25 @@
-import { Module } from 'vuex'
+import { Module, MutationTree } from 'vuex'
 import { GlobalState } from '../index'
+import { TOGGLE_COLLAPSE } from '../mutation-types'
 
 export interface AppState {
   isCollapse: boolean
 }
 
+const appState: AppState = {
+  isCollapse: false,
+}
+
+const appMutations: MutationTree<AppState> = {
+  [TOGGLE_COLLAPSE]: (state) => {
+    state.isCollapse = !state.isCollapse
+  },
+}
+
 export const app: Module<AppState, GlobalState> = {
   namespaced: true,
 
-  state: {
-    isCollapse: false,
-  },
+  state: appState,
+
+  mutations: appMutations,
 }
